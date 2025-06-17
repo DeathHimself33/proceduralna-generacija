87adb21a4f949ec6ec5a47e2ac3f372e38cc4194
frarthy <annansharonova@gmail.com>
2025-06-17 16:54:52 +0200

    finalcode

--- fragmentShader.glsl
+++ fragmentShader.glsl
@@ -4,15 +4,15 @@ varying float vVerticalPos;
 uniform float uLayerThreshold;
 
 void main() {
-    vec4 gray = vec4(0.2, 0.7, 0.3, 1.0);
+    vec4 green = vec4(0.2, 0.7, 0.3, 1.0);
     vec4 coffee = vec4(0.44, 0.31, 0.22, 1.0);
-    vec4 green = vec4(0.5, 0.5, 0.5, 1.0);
+    vec4 water  = vec4(0.2, 0.5, 0.8, 1.0);
     
     if (vVerticalPos > uLayerThreshold) {
-        gl_FragColor = gray;
+        gl_FragColor = green;
     } else if (vVerticalPos > -uLayerThreshold) {
         gl_FragColor = coffee;
     } else {
-        gl_FragColor = green;
+        gl_FragColor = water;
     }
 }
